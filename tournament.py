fb7106ee9d86bf71365bf0cdcf9c0228f4e0e1fa
ProfiSketch <profisketch@users.noreply.github.com>
2024-05-06 16:58:32 +0300

    feat: :building_construction: Добавление игроков, улучшение отчёта

--- tournament.py
+++ tournament.py
@@ -12,96 +12,32 @@ class Tournament():
         
         # Турнирная таблица
         self.statistics = pd.DataFrame()
-    
-        # Создаем игроков
+            
+        # Создаем массив для хранения игроков
+        self.playerSet = []
+
+        # Параметры игроков (имя, порог, стратегия)
+        player_params = [
+            ("Прозорливый {}".format(i), i, 'CautiousStrategy') for i in range(0, 170, 5)
+        ]
+
+        # Создаем прозорливых игроков с помощью цикла
+        for name, threshold, strategy in player_params:
+            player = Player(
+                name=name,
+                strategy=strategy,
+                threshold=threshold
+            )
+            self.playerSet.append(player)
+
+        # Создаем жадного игрока
         playerA = Player(
             name="Жадный",
             strategy='GreedyStrategy'
-            
         )
-        
-        playerB = Player(
-            name="Прозорливый 5",
-            strategy='CautiousStrategy',
-            threshold=5
-        )
-        
-        playerC = Player(
-            name="Прозорливый 10",
-            strategy='CautiousStrategy',
-            threshold=10
-        )        
-        
-        playerD = Player(
-            name="Прозорливый 15",
-            strategy='CautiousStrategy',
-            threshold=15
-        )
-        
-        playerE = Player(
-            name="Прозорливый 20",
-            strategy='CautiousStrategy',
-            threshold=20
-        )
-        
-        playerF = Player(
-            name="Прозорливый 30",
-            strategy='CautiousStrategy',
-            threshold=30
-        )
-        
-        playerG = Player(
-            name="Прозорливый 40",
-            strategy='CautiousStrategy',
-            threshold=40
-        )
-                
-        playerH = Player(
-            name="Прозорливый 50",
-            strategy='CautiousStrategy',
-            threshold=50
-        )
-                        
-        playerJ = Player(
-            name="Прозорливый 60",
-            strategy='CautiousStrategy',
-            threshold=60
-        )
-                        
-        playerK = Player(
-            name="Прозорливый 80",
-            strategy='CautiousStrategy',
-            threshold=80
-        )
-                        
-        playerL = Player(
-            name="Прозорливый 100",
-            strategy='CautiousStrategy',
-            threshold=100
-        )
-                        
-        playerM = Player(
-            name="Прозорливый 110",
-            strategy='CautiousStrategy',
-            threshold=110
-        )
-        
-        # Добавляем их в массив
-        self.playerSet = [
-            playerA,
-            playerB,
-            playerC,
-            playerD,
-            playerE,
-            playerF,
-            playerG,
-            playerH,
-            playerJ,
-            playerK,
-            playerL,
-            playerM,
-        ]
-        
+
+        self.playerSet.append(playerA)
+ 
     
     def start(self):
         ''' Запуск турнира '''
@@ -137,10 +73,12 @@ class Tournament():
     def report(self):
         ''' Печать отчёта '''
         
-        print(self.statistics)
-        
         # Группируем данные по игрокам и считаем количество уникальных значений в столбце "winner"
         winner_count = self.statistics.groupby('winner').size()
 
         print("Количество выигранных партий каждым игроком:")
-        print(winner_count.sort_values(ascending=False))
+        total_games = winner_count.sum()  # Общее количество игр
+
+        # Вычисляем процентное соотношение, округляем до десятых и выводим результат
+        winner_percentage = (winner_count / total_games * 100).round(decimals=1)
+        print(winner_percentage.sort_values(ascending=False))
